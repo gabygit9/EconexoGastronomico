d4f3397d3d44365c227a1eadd20db9bd0303d889
gabygit9 <gabrielacamacho09@hotmail.com>
2026-06-06 08:24:29 -0300

    Update dashboards

--- Frontend/src/app/features/dashboard/dashboard-donor/dashboard-donor.component.ts
+++ Frontend/src/app/features/dashboard/dashboard-donor/dashboard-donor.component.ts
@@ -5,12 +5,15 @@ import {FooterComponent} from '../../../shared/components/footer/footer.componen
 import {NavbarComponent} from '../../../shared/components/navbar/navbar.component';
 import {DriverResponse} from '../../../shared/models/driver.model';
 import {DonorResponse} from '../../../shared/models/donor.model';
+import {map} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-donor',
   imports: [
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    AsyncPipe
   ],
   templateUrl: './dashboard-donor.component.html',
   styleUrl: './dashboard-donor.component.css'
@@ -18,19 +21,16 @@ import {DonorResponse} from '../../../shared/models/donor.model';
 export class DashboardDonorComponent implements OnInit{
   private readonly authService = inject(AuthService);
 
-  donorProfile: DonorResponse | null = null;
-  isLoading = true;
+  userName$ = this.authService.currentUser$.pipe(
+    map(profile => {
+      if(profile && 'tradeName' in profile){
+        return profile;
+      }
+      return '';
+    })
+  );
 
   ngOnInit(){
-    this.authService.getDonorProfile().subscribe({
-      next: (profile) => {
-        this.donorProfile = profile;
-        this.isLoading = false;
-      },
-      error: (error) => {
-        console.error(error);
-        this.isLoading = false;
-      }
-    })
+
   }
 }
